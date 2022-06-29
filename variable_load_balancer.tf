7e62260a2bcb6111fc2fe8496850d8198e9c60dd
ReneeP25 <renee.seles@gmail.com>
2022-06-29 18:58:38 +0530

    azure load balancer

---
+++ variable_load_balancer.tf
@@ -0,0 +1,29 @@
+variable "rgname" {
+  default = "resource_group1"
+}
+
+variable "location" {
+    default = "eastus"
+}
+
+variable "publicip" {
+    default = "public_ip1" 
+}
+
+variable "allocation_method" {
+    default = "Static"
+}
+
+variable "lb" {
+    default = "loadbalancer1"
+  
+}
+
+variable "sku" {
+    default = "Basic"
+}
+
+variable "frontend_config" {
+    default = "frontend_ip_address"
+  
+}
